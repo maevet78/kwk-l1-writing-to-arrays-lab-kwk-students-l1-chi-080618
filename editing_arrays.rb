f3425d815d78990a57aa3c47f03ffdf9db698bd5
maevet78 <190439mt@saintviator.com>
2018-08-08 15:35:34 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -4,11 +4,14 @@ def change_rainbow_colors
   RAINBOW_COLORS.push[0,"red"]
   RAINBOW_COLORS.push[1,"light red"]
   RAINBOW_COLORS.push[2,"light yellow"]
+  return RAINBOW_COLORS
 end
+puts change_rainbow_colors
 
 def add_colors
   RAINBOW_COLORS.insert[3,"green"]
   RAINBOW_COLORS.insert[4,"blue"]
+  return RAINBOW_COLORS
 end
+puts add_colors
 
-return RAINBOW_COLORS

d0bfa49697e65fb92ae4857f2aa14b94a69759d7
maevet78 <190439mt@saintviator.com>
2018-08-08 15:38:28 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -1,9 +1,9 @@
 RAINBOW_COLORS = ["yellow", "default", "light_cyan"]
 
 def change_rainbow_colors
-  RAINBOW_COLORS.push[0,"red"]
-  RAINBOW_COLORS.push[1,"light red"]
-  RAINBOW_COLORS.push[2,"light yellow"]
+  RAINBOW_COLORS[0,"red"]
+  RAINBOW_COLORS[1,"light red"]
+  RAINBOW_COLORS[2,"light yellow"]
   return RAINBOW_COLORS
 end
 puts change_rainbow_colors
